1498629e7ba7892590ea2e8b9c356487661d9195
dinesh1988 <dinesh_soundar@yahoo.com>
2018-05-19 19:43:03 -0400

    Summary
    src changed

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularTemplatePage } from './app.po';
+import { FirestarterPage } from './app.po';
 
-describe('angular-template App', () => {
-  let page: AngularTemplatePage;
+describe('firestarter App', () => {
+  let page: FirestarterPage;
 
   beforeEach(() => {
-    page = new AngularTemplatePage();
+    page = new FirestarterPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
