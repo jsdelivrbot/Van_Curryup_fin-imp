70e8f56c466cd5e48439ad5473ed3d8bf53e5a26
dinesh1988 <dinesh_soundar@yahoo.com>
2018-05-19 22:07:31 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyCaJdxLI7VqTl8VYLFqiG--37uQ-pR3tK8",
-        authDomain: "pwa-app-din.firebaseapp.com",
-        databaseURL: "https://pwa-app-din.firebaseio.com",
-        projectId: "pwa-app-din",
-        storageBucket: "pwa-app-din.appspot.com",
-        messagingSenderId: "975578376262"
+       
     }
 };
